94f7604e693734e653bb8b3088cb3ba28fc35750
Kanisk Chakraborty <57884127+chakrakan@users.noreply.github.com>
2021-02-24 21:32:34 -0500

    chore: add deps + clean up

--- src/index.ts
+++ src/index.ts
@@ -12,31 +12,33 @@
  * @public
  */
 export function avg(a: number, b: number, c: number): number {
-    return sum3(a, b, c) / 3;
-  }
-  
-  /**
-   * Calculate the sum of three numbers
-   *
-   * @param a - first number
-   * @param b - second number
-   * @param c - third number
-   *
-   * @beta
-   */
-  export function sum3(a: number, b: number, c: number): number {
-    return sum2(a, sum2(b, c));
-  }
-  
-  /**
-   * Calculate the sum of two numbers
-   *
-   * @param a - first number
-   * @param b - second number
-   *
-   * @internal
-   */
-  export function sum2(a: number, b: number): number {
-    const sum = a + b;
-    return sum;
-  }
+  return sum3(a, b, c) / 3;
+}
+
+/**
+ * Calculate the sum of three numbers
+ *
+ * @param a - first number
+ * @param b - second number
+ * @param c - third number
+ *
+ * @beta
+ */
+export function sum3(a: number, b: number, c: number): number {
+  return sum2(a, sum2(b, c));
+}
+
+/**
+ * Calculate the sum of two numbers
+ *
+ * @param a - first number
+ * @param b - second number
+ *
+ * @internal
+ */
+export function sum2(a: number, b: number): number {
+  const sum = a + b;
+  return sum;
+}
+
+console.log(`Hello World, the meaning of life is ${sum3(38, 1, 3)}`);
